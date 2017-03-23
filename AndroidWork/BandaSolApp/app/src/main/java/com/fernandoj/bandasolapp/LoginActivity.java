2b0d366651e6e5b20216458d25d80da236df5308
fdezfernando <fjfearroyo@gmail.com>
2017-03-23 13:59:01 +0100

    Update Android

---
+++ AndroidWork/BandaSolApp/app/src/main/java/com/fernandoj/bandasolapp/LoginActivity.java
@@ -0,0 +1,35 @@
+package com.fernandoj.bandasolapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class LoginActivity extends AppCompatActivity {
+
+    EditText editTextUsername, editTextPassword;
+    Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+        editTextUsername = (EditText) findViewById(R.id.edit_text_username);
+        editTextPassword = (EditText) findViewById(R.id.edit_text_password);
+        btn = (Button) findViewById(R.id.button_login);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(LoginActivity.this, MainActivity.class );
+                startActivity(i);
+            }
+        });
+    }
+
+
+}
